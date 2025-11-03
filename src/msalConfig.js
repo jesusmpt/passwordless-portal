0a51f741f9212c82582ebaa8234a841ae373b874
jesusmpt <52034316+jesusmpt@users.noreply.github.com>
2025-11-03 22:45:58 +0100

    Update msalConfig.js

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -1,12 +1,7 @@
-const msalConfig = {
+export const msalConfig = {
   auth: {
     clientId: "b151075c-0779-42c2-b7aa-fa1bc6c36bd5",
     authority: "https://login.microsoftonline.com/9ff87f7c-8358-46b5-88bc-d73c09ce789f",
-    redirectUri: "https://jolly-beach-0c9a28403.3.azurestaticapps.net"
+    redirectUri: "https://orange-bush-0ee8dca03.3.azurestaticapps.net",
   },
-  cache: {
-    cacheLocation: "sessionStorage",
-    storeAuthStateInCookie: false
-  }
 };
-export default msalConfig;
